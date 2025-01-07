5d3afa1fa83580fafbd8dda081d94d5a7ae68a09
freddedotme-v2 <code@fredde.me>
2025-01-07 22:33:09 +0100

    added 8ball logic

--- bot.go
+++ bot.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -15,6 +17,7 @@ func main() {
 	}
 
 	discord.AddHandler(ready)
+	discord.AddHandler(messageCreate)
 
 	err = discord.Open()
 	if err != nil {
@@ -23,5 +26,46 @@ func main() {
 }
 
 func ready(s *discordgo.Session, event *discordgo.Ready) {
-	s.UpdateGameStatus(0, "relaxing")
+	s.UpdateGameStatus(0, "!8ball")
+}
+
+func messageCreate(s *discordgo.Session, event *discordgo.MessageCreate) {
+	// Ignore if the author is the bot, to avoid infinite loop.
+	if event.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// Discard if it doesn't have the correct prefix.
+	if !strings.HasPrefix(event.Content, "!8ball ") {
+		return
+	}
+
+	answers := []string{
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As I see it, yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy, try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful",
+	}
+
+	_, err := s.ChannelMessageSend(event.ChannelID, answers[rand.Intn(len(answers))])
+	if err != nil {
+		fmt.Printf("Error while sending: %s\n", err)
+		return
+	}
 }
